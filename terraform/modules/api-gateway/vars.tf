ef2195fcf712c6218afa87a84a6b8dd035a6f400
Garrett Filippini <gfilippini4@gmail.com>
2025-02-24 23:50:04 -0700

    Cleaning up a few things

--- terraform/modules/api-gateway/vars.tf
+++ terraform/modules/api-gateway/vars.tf
@@ -12,3 +12,9 @@ variable "base_backend_lambda_invoke_arn" {
   type        = string
   description = "ARN of the lambd that we are invoking"
 }
+
+variable "enable_vpc" {
+  type        = bool
+  description = "Boolean value if we should lock down the gateway to the internal VPC"
+  default     = false
+}
