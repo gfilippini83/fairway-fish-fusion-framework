e53338432bf8f04b951fe0baad0928c8165fc184
Garrett Filippini <gfilippini4@gmail.com>
2025-02-25 10:59:26 -0700

    Pushing some updates

--- framework/lambdas/base-backend-lambda/index.ts
+++ framework/lambdas/base-backend-lambda/index.ts
@@ -1,7 +1,9 @@
+import { APIGatewayProxyEvent } from 'aws-lambda'
+
 export const handler = async (
-    event: any
+    event: APIGatewayProxyEvent
 ): Promise<any> => {
-    const message = "Hello World!";
+    const message = "Hello World! Update from Garrett";
     console.log(`Returning ${message}`);
     return {
         statusCode: 200,
